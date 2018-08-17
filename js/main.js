5511a76dbefac6608e6c36aaf64eb84516eca2a4
Fausto Brito <fgbritom79@hotmail.com>
2018-08-17 11:55:05 -0400

    adding logic

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.getElementById("text").innerHTML = "this is a text";
+document.getElementById("hello").innerHTML = "hello world";
+document.getElementById("calc").innerHTML = 5 + 5;
+
+ function showAlertBox() {
+   alert("display message");
+ }
+
+ function displayName(name) {
+   alert("This is my name: " + name);
+ }
+
+ function showMessage() {
+   console.log("show message");
+ }
+ function theSumOfNumbers() {
+   var number_one = 6;
+   var number_two = 5;
+   var total = number_one + number_two;
+   alert("The total sum of numbers are: " + total);
+ }
+
+function multiply(a, b) {
+  var result;
+  var result = a * b;
+
+  alert("Result: " + result);
+}
